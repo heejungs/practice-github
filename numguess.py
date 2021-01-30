43b9080d1ee62dd39c0710e508ae9b23b663bf3b
heejungs <hee980313@gmail.com>
2021-01-30 11:00:03 +0900

    change game's rule

--- numguess.py
+++ numguess.py
@@ -2,12 +2,12 @@
 이 게임은 numguess게임으로 사용자가 컴퓨터가 낸 랜덤한 값을 맞추는 게임입니다
 """
 import random
-
-while True:
-    guess = int(input())
-    if guess == 0:
-        break       
+cnt = 5
+while cnt != 0:
     num1 = random.randint(1,100)
-    print(num1)
+    guess = int(input())
 
-
+    if guess != num1:
+        print('NOPE !')
+        cnt -=1
+print('GAME OVER')  
