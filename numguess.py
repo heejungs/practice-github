23dab8b4aa1b05ac407179ec5e192aff92a921f2
heejungs <hee980313@gmail.com>
2021-01-30 10:56:35 +0900

    feat: output answer

--- numguess.py
+++ numguess.py
@@ -2,9 +2,11 @@ import random
 
 while True:
     guess = int(input())
-    if guess == 0:
+    if guess ==0:
+        print('OK BYEBYE SEE YOU LATER.')
         break       
     num1 = random.randint(1,100)
-    print(num1)
+    print(f'ANSWER : {num1}')
+    print(f'YOUR ANSWER : {guess}')
 
 
