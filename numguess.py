b6d2df4aefa24e196664b8c0181f97da81e9eccd
heejungs <hee980313@gmail.com>
2021-01-30 10:49:04 +0900

    feat: add while and add user's input

--- numguess.py
+++ numguess.py
@@ -1,4 +1,10 @@
 import random
 
-num1 = random.randint(1,100)
-print(num1)
+while True:
+    guess = int(input())
+    if guess == 0:
+        break       
+    num1 = random.randint(1,100)
+    print(num1)
+
+
